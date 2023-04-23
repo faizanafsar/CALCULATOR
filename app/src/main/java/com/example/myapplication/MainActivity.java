5e9274ad9c40f7aa0d527ad1fd05613a9868d3c2
FAIZAN AFSAR <faizanafsar782@gmail.com>
2023-04-23 16:59:22 +0500

    IN PROCESS

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText numE1, numE2;
+    Button addBtn, subBtn;
+    TextView resultView;
+    int n1, n2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        numE1 = (EditText) findViewById(R.id.editText1);
+        numE2 = (EditText) findViewById(R.id.editText2);
+
+        addBtn = (Button) findViewById(R.id.addButton);
+        addBtn.setOnClickListener(this);
+        subBtn = (Button) findViewById(R.id.subButton);
+        subBtn.setOnClickListener(this);
+        resultView = (TextView) findViewById(R.id.resultTV);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        n1 = Integer.parseInt(numE1.getText().toString());
+        n2 = Integer.parseInt(numE2.getText().toString());
+        switch (view.getId()){
+            case R.id.addButton:
+                resultView.setText("answer is "+ (n1 + n2));
+                break;
+            case R.id.subButton:
+                resultView.setText("answer is "+ (n1 - n2));
+                break;
+            default:
+                break;
+
+        }
+
+    }
+}
